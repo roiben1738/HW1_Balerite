58feaa7bb4f17d01d92abe3b9c1dc14394c409a6
roiben1738 <57590979+roiben1738@users.noreply.github.com>
2019-11-11 03:21:26 +0800

    Add files via upload

---
+++ Balerite_ConsonantVowel.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+char = input("Please Enter Character : ")
+
+if(char == 'a' or char == 'i' or char == 'o' or char == 'u' or char == 'U' or char == 'A' or char == 'E' or char == 'I' or char == 'O' ):
+    print ("The Character",char, "is a Vowel")
+else:
+    print ("The Character" ,char, "is a Consonant")
+    
